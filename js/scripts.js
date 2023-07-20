ce100926d21a0fc589cb9b8d4b1e63c63999fd7a
larrobbal <robb.strike@gmail.com>
2023-07-20 12:02:32 -0600

    Agregada funcionalidad de menú lateral. Arreglado distribución footer

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ import {showModal} from "./validaciones.js";
 import {closeModal} from "./validaciones.js";
 
 (() => {
+    const mobileMenuToggle = document.querySelector("#mobile__menu__button");
     const closeModalBtn = document.querySelector("#modal__close__button");
     const form = document.getElementById("contact__form");
+    const mobileMenu = document.getElementById("mobile__menu");
+    const mobileMenuOptions = mobileMenu.querySelectorAll(":scope > li > a");
 
     const inputs = form.querySelectorAll(":scope > div > .input__form");
     const sendButton = form.querySelector("#send__button");
@@ -25,5 +28,38 @@ import {closeModal} from "./validaciones.js";
     closeModalBtn.addEventListener("click", (closeModalBtn) => {
         closeModalBtn.preventDefault();
         closeModal(inputs);
+    });
+
+    mobileMenuToggle.addEventListener("click", (mobileMenuToggle) => {
+        const menuToggle = document.querySelector("#mobile__menu__button");
+        const mobileMenu = document.querySelector("#mobile__menu");
+        if(!menuToggle.classList.contains("is-active"))
+        {
+            menuToggle.classList.add("is-active");
+            mobileMenu.style.left="0px";
+        }
+            
+        else
+        {
+            menuToggle.classList.remove("is-active");
+            mobileMenu.style.left="-300px";
+        }
+    });
+    mobileMenuOptions.forEach(option => {
+        option.addEventListener("click", ()=>{
+            const menuToggle = document.querySelector("#mobile__menu__button");
+            const mobileMenu = document.querySelector("#mobile__menu");
+            if(!menuToggle.classList.contains("is-active"))
+            {
+                menuToggle.classList.add("is-active");
+                mobileMenu.style.left="0px";
+            }
+                
+            else
+            {
+                menuToggle.classList.remove("is-active");
+                mobileMenu.style.left="-300px";
+            }
+        })
     });
 })();
